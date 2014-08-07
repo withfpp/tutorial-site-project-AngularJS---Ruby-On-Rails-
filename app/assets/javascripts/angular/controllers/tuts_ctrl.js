b8050d8fa696d3279c30e394cb502706a14f9a66
tk <withfpp@gmail.com>
2014-08-07 14:51:48 +0900

    ng-click event to list

--- app/assets/javascripts/angular/controllers/tuts_ctrl.js
+++ app/assets/javascripts/angular/controllers/tuts_ctrl.js
@@ -1,3 +1,9 @@
 myApp.controller('TutsCtrl', ['$scope','myFactory',function($scope,myFactory){
 	$scope.tuts = myFactory.query();
+	$scope.selectedTut = null;
+
+	//set the selected tutorial to the view
+	$scope.showTuts = function(tut){
+		$scope.selectedTut = tut;
+	}
 }]);
